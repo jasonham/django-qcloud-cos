cd6b7ca0b683bf4644b3da001ff58336c7746714
jasonham <jasonham.cn@outlook.com>
2017-03-08 03:33:34 +0800

    finish with xml api authorization and test

--- QcloudCos/cos_auth.py
+++ QcloudCos/cos_auth.py
@@ -8,7 +8,7 @@ import base64
 
 
 class Auth(object):
-    def __init__(self, appid, SecretID, SecretKey, bucket, region, method, objectName, head="", parameters=""):
+    def __init__(self, appid, SecretID, SecretKey, bucket, region, method, objectName, head="", parameters="", sign_time="", key_time=""):
         """
 
         :param appid: 开发者访问 COS 服务时拥有的用户维度唯一资源标识，用以标示资源。
@@ -20,6 +20,8 @@ class Auth(object):
         :param objectName: 指该请求中的 URI 部分，即除去 http:// 协议和域名的部分（通常以 / 开始），并且不包含 URL 中的参数部分（通常以 ? 开始）。
         :param head: 指请求中的 HTTP 头部信息，用 key=value 的方式表达。头部的 key 必须全部小写，value 必须经过 URL Encode。如果有多个参数对可使用 & 连接。key值按字典序排序
         :param parameters: 指该请求中的参数部分（以 ? 开始的部分），用 key=value 的方式表达。
+        :param sign_time: 无需指定 签名的有效起止时间，其使用 10 位 Unix 时间戳来表示，有效效力精确到秒。该字段通过分号区分起止，起时在前、止时在后。
+        :param key_time: 无需指定 用户可以自定义 SignningKey 有效时间，使用 10 位 Unix 时间戳来表示，有效效力精确到秒。该字段通过分号区分起止，起始时间在前、终止时间在后。一般 q-key-time 的时间范围大于等于 q-sign-time。
         """
         self.appid = appid
         self.SecretID = SecretID
@@ -32,54 +34,89 @@ class Auth(object):
         self.parameters = parameters.encode('utf-8')
         self.head = head
 
-        self.q_sign_time = int(time.time())
-        self.q_key_time = self.q_sign_time + 60
+        if sign_time == "":
+            self.q_sign_time = int(time.time())
+        else:
+            self.q_sign_time = int(sign_time)
+
+        if key_time == "":
+            self.q_key_time = self.q_sign_time + 60
+        else:
+            self.q_key_time = int(key_time)
+
         self.sh1hash_formatstring = ''
 
     def get_signkey(self):
         q_time = '%s;%s' % (self.q_sign_time, self.q_key_time)
-        signkey = hmac.new(bytes(self.SecretKey,'utf-8'), bytes(q_time,'utf-8'), hashlib.sha1).hexdigest()
+        signkey = hmac.new(bytes(self.SecretKey, 'utf-8'), bytes(q_time, 'utf-8'), hashlib.sha1).hexdigest()
         return signkey
 
-    def set_sh1hash_formatstring(self):
+    def format_args(self, args, isfullstring=True, isheader=True):
+        list_temp = []
+        if isheader:
+            str_temp = '%s-%s.%s.myqcloud.com' % (self.bucket, self.appid, self.region)
+            list_temp = [('host', quote(str_temp, encoding='utf-8').lower())]
+
+        try:
+            args_list = args.split('&')
+        except:
+            args_list = args
+
+        for n in args_list:
+            arg = n.split(':')
+            list_temp.append((arg[0].lower(), quote(arg[1], encoding='utf-8').lower()))
+
+        list_temp.sort(key=lambda x:x[0])
+        my_f_args = ''
+        if isfullstring:
+            for n in list_temp:
+                my_f_args += n[0] + '=' + n[1] + '&'
+        else:
+            for n in list_temp:
+                my_f_args += n[0] + ';'
+
+        my_f_args = my_f_args[0:-1]
+        return my_f_args
+
+    def get_formatstring(self):
         format_method = self.method
         format_url = self.objectname
-        format_parameters = self.parameters
+        format_parameters = self.format_args(self.parameters,True,False)
 
-        host = '%s-%s.%s.myqcloud.com' % (self.bucket, self.appid, self.region)
-        format_head = {'host': host.encode('utf-8')}
-        if self.head != "":
-            headlist = self.head.split('&')
-            for n in headlist:
-                head = n.split('=')
-                newlist = {head[0].lower(): head[1].encode('utf-8')}
-                format_head.update(newlist)
+        format_head = self.format_args(self.head)
+        format_string = "%s\n%s\n%s\n%s\n" % (format_method, format_url, format_parameters, format_head)
 
-
-
-
-        format_string = "%s\n%s\n%s\n%s" % (format_method, format_url, format_parameters, format_headers)
-        self.sh1hash_formatstring = hashlib.sha1(format_string)
+        return format_string
 
     def get_stringtosign(self):
-        q_sign_time = '%s;%s' % (self.q_sign_time, self.q_sign_time)
-        stringtosign = "sha1\n%s\n%s" % (q_sign_time, self.sh1hash_formatstring)
+        q_sign_time = '%s;%s' % (self.q_sign_time, self.q_key_time)
+        formatstring = self.get_formatstring()
+        sh1hash_formatstring = hashlib.sha1(formatstring.encode('utf-8')).hexdigest()
+        stringtosign = "sha1\n%s\n%s\n" % (q_sign_time, sh1hash_formatstring)
         return stringtosign
 
     def get_signature(self):
-        signature = hmac.new(bytes(self.get_signkey()), bytes(self.get_stringtosign()), hashlib.sha1).hexdigest()
+        signature = hmac.new(
+            bytes(self.get_signkey(), 'utf-8'),
+            bytes(self.get_stringtosign(), 'utf-8'),
+            hashlib.sha1
+        ).hexdigest()
         return signature
 
     def get_authorization(self):
-        signed_header_list = "host"
-        t_tuple = (self.SecretID, self.q_sign_time, self.q_key_time, )
+        header_list = self.format_args(self.head,False)
+        signed_parameter = self.format_args(self.parameters,False,False)
+        q_time = '%s;%s' % (self.q_sign_time, self.q_key_time)
+
+        t_tuple = (self.SecretID, q_time, q_time, header_list, signed_parameter, self.get_signature())
         authorization = "q-sign-algorithm=sha1&" \
                         "q-ak=%s&" \
                         "q-sign-time=%s&" \
                         "q-key-time=%s&" \
-                        "q-header-list=<SignedHeaderList>&" \
-                        "q-url-param-list=<SignedParameterList>&" \
-                        "q-signature=<Signature>" % t_tuple
+                        "q-header-list=%s&" \
+                        "q-url-param-list=%s&" \
+                        "q-signature=%s" % t_tuple
+        return authorization
 
 
     # def __init__(self, appid, SecretID, SecretKey, bucket, file='', currentTime='', expiredTime='', rand=''):
@@ -120,3 +157,4 @@ class Auth(object):
     #     sign = base64.b64encode(SignTmp + original).decode('utf-8')
     #
     #     return sign
+
--- test/test_cos_auth.py
+++ test/test_cos_auth.py
@@ -4,46 +4,109 @@ import unittest
 
 class cos_auth_TestCase(unittest.TestCase):
     def setUp(self):
-        self.multi = cos_auth.Auth(appid='200001',
-                               SecretID='AKIDUfLUEUigQiXqm7CVSspKJnuaiIKtxqAv',
-                               SecretKey='bLcPnl88WU30VY57ipRhSePfPdOfSruK',
-                               bucket='newbucket',
-                               file='',
-                               currentTime=1437995644,
-                               expiredTime=1437995704,
-                               rand=2081660421)
-        self.once = cos_auth.Auth(appid='200001',
-                                   SecretID='AKIDUfLUEUigQiXqm7CVSspKJnuaiIKtxqAv',
-                                   SecretKey='bLcPnl88WU30VY57ipRhSePfPdOfSruK',
-                                   bucket='newbucket',
-                                   file='tencent_test.jpg',
-                                   currentTime=1437995644,
-                                   expiredTime=1437995704,
-                                   rand=2081660421)
-
-    def test_multi_effect_original(self):
-        self.assertEqual(
-            self.multi.get_original(),
-            'a=200001&b=newbucket&k=AKIDUfLUEUigQiXqm7CVSspKJnuaiIKtxqAv&e=1437995704&t=1437995644&r=2081660421&f='
+        self.maxDiff = None
+        self.get_xml_api = cos_auth.Auth(
+            appid='125000000',
+            SecretID='QmFzZTY0IGlzIGEgZ2VuZXJp',
+            SecretKey='AKIDZfbOA78asKUYBcXFrJD0a1ICvR98JM',
+            bucket='testbucket',
+            region='cn-north',
+            method='get',
+            objectName='/testfile',
+            head="Range:bytes=0-3",
+            sign_time="1480932292",
+            key_time="1481012292"
+        )
+        self.put_xml_api = cos_auth.Auth(
+            appid='125000000',
+            SecretID='QmFzZTY0IGlzIGEgZ2VuZXJp',
+            SecretKey='AKIDZfbOA78asKUYBcXFrJD0a1ICvR98JM',
+            bucket='testbucket',
+            region='cn-north',
+            method='put',
+            objectName='/testfile2',
+            head="x-cos-content-sha1:db8ac1c259eb89d4a131b253bacfca5f319d54f2&x-cos-stroage-class:nearline",
+            sign_time="1480932292",
+            key_time="1481012292"
         )
 
-    def test_multi_effect_signature(self):
+    def test_get_xml_api_SignKey(self):
+        self.assertEqual(self.get_xml_api.get_signkey(),'95d110a8ead64cac52083100db75b7e3f369e72f')
+
+    def test_get_xml_api_FormatString(self):
+        b = "get\n/testfile\n\nhost=testbucket-125000000.cn-north.myqcloud.com&range=bytes%3d0-3\n"
+        self.assertEqual(self.get_xml_api.get_formatstring(),b
+                         )
+
+    def test_get_xml_api_StringToSign(self):
+        a = self.get_xml_api.get_stringtosign()
+        b = 'sha1\n1480932292;1481012292\nc92f7246e3f922fe4abae5d6d5ebcd2397dc88cb\n'
+        self.assertEqual(a,b)
+
+    def test_get_xml_api_Signature(self):
+        a = self.get_xml_api.get_signature()
+        b = '29b2f454bb9d8a629e7cad61227bd5fd0dd11a2d'
+        self.assertEqual(a, b)
+
+    def test_get_xml_api_Authorization(self):
+        a = self.get_xml_api.get_authorization()
+        b = 'q-sign-algorithm=sha1&q-ak=QmFzZTY0IGlzIGEgZ2VuZXJp&q-sign-time=1480932292;1481012292&q-key-time=1480932292;1481012292&q-header-list=host;range&q-url-param-list=&q-signature=29b2f454bb9d8a629e7cad61227bd5fd0dd11a2d'
         self.assertEqual(
-            self.multi.get_sign(),
-            'wHgvVkXpcZ3d+G0rOkAJml3R5NdhPTIwMDAwMSZiPW5ld2J1Y2tldCZrPUFLSURVZkxVRVVpZ1FpWHFtN0NWU3NwS0pudWFpSUt0eHFBdiZlPTE0Mzc5OTU3MDQmdD0xNDM3OTk1NjQ0JnI9MjA4MTY2MDQyMSZmPQ=='
+            a,
+            b
         )
 
-    def test_once_original(self):
-        self.assertEqual(
-            self.once.get_original(),
-            'a=200001&b=newbucket&k=AKIDUfLUEUigQiXqm7CVSspKJnuaiIKtxqAv&e=0&t=1437995644&r=2081660421&f=/200001/newbucket/tencent_test.jpg'
-        )
 
-    def test_once_signature(self):
+
+    def test_put_xml_api_Authorization(self):
+        a = self.put_xml_api.get_authorization()
+        b = 'q-sign-algorithm=sha1&q-ak=QmFzZTY0IGlzIGEgZ2VuZXJp&q-sign-time=1480932292;1481012292&q-key-time=1480932292;1481012292&q-header-list=host;x-cos-content-sha1;x-cos-stroage-class&q-url-param-list=&q-signature=b237c36c5495b048519b82b17a200840594c0339'
         self.assertEqual(
-            self.once.get_sign(),
-            'rUbXBVKcp4tdZKkm5wz6//DqLIdhPTIwMDAwMSZiPW5ld2J1Y2tldCZrPUFLSURVZkxVRVVpZ1FpWHFtN0NWU3NwS0pudWFpSUt0eHFBdiZlPTAmdD0xNDM3OTk1NjQ0JnI9MjA4MTY2MDQyMSZmPS8yMDAwMDEvbmV3YnVja2V0L3RlbmNlbnRfdGVzdC5qcGc='
+            a,
+            b
         )
+    #     self.multi = cos_auth.Auth(appid='200001',
+    #                            SecretID='AKIDUfLUEUigQiXqm7CVSspKJnuaiIKtxqAv',
+    #                            SecretKey='bLcPnl88WU30VY57ipRhSePfPdOfSruK',
+    #                            bucket='newbucket',
+    #                            file='',
+    #                            currentTime=1437995644,
+    #                            expiredTime=1437995704,
+    #                            rand=2081660421)
+    #     self.once = cos_auth.Auth(appid='200001',
+    #                                SecretID='AKIDUfLUEUigQiXqm7CVSspKJnuaiIKtxqAv',
+    #                                SecretKey='bLcPnl88WU30VY57ipRhSePfPdOfSruK',
+    #                                bucket='newbucket',
+    #                                file='tencent_test.jpg',
+    #                                currentTime=1437995644,
+    #                                expiredTime=1437995704,
+    #                                rand=2081660421)
+    #
+    #
+    # def test_multi_effect_original(self):
+    #     self.assertEqual(
+    #         self.multi.get_original(),
+    #         'a=200001&b=newbucket&k=AKIDUfLUEUigQiXqm7CVSspKJnuaiIKtxqAv&e=1437995704&t=1437995644&r=2081660421&f='
+    #     )
+    #
+    # def test_multi_effect_signature(self):
+    #     self.assertEqual(
+    #         self.multi.get_sign(),
+    #         'wHgvVkXpcZ3d+G0rOkAJml3R5NdhPTIwMDAwMSZiPW5ld2J1Y2tldCZrPUFLSURVZkxVRVVpZ1FpWHFtN0NWU3NwS0pudWFpSUt0eHFBdiZlPTE0Mzc5OTU3MDQmdD0xNDM3OTk1NjQ0JnI9MjA4MTY2MDQyMSZmPQ=='
+    #     )
+    #
+    # def test_once_original(self):
+    #     self.assertEqual(
+    #         self.once.get_original(),
+    #         'a=200001&b=newbucket&k=AKIDUfLUEUigQiXqm7CVSspKJnuaiIKtxqAv&e=0&t=1437995644&r=2081660421&f=/200001/newbucket/tencent_test.jpg'
+    #     )
+    #
+    # def test_once_signature(self):
+    #     self.assertEqual(
+    #         self.once.get_sign(),
+    #         'rUbXBVKcp4tdZKkm5wz6//DqLIdhPTIwMDAwMSZiPW5ld2J1Y2tldCZrPUFLSURVZkxVRVVpZ1FpWHFtN0NWU3NwS0pudWFpSUt0eHFBdiZlPTAmdD0xNDM3OTk1NjQ0JnI9MjA4MTY2MDQyMSZmPS8yMDAwMDEvbmV3YnVja2V0L3RlbmNlbnRfdGVzdC5qcGc='
+    #     )
+    #
 
     def tearDown(self):
         self.auth=None
